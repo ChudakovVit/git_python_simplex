3b8dc9062273b2cc6df2205f862d1a87df1f81ec
Chudakov <ChudakovVit@yandex.ru>
2017-01-10 00:20:15 +0300

    work solve simplex

--- main.py
+++ main.py
@@ -2,20 +2,62 @@ import sys
 from scipy.optimize import linprog
 from PyQt5.QtWidgets import *
 
+
+objective_function = [-1, 10, -1]
+constraints = [[1, -5.5, -7], [1, 14.5, 7]]
+constraints_equal = [-13, 15]
+
+# objective_function = [-1, -2, -3, 4]
+# constraints = [[1, 1, -1, 1], [1, 14, 10, -10]]
+# constraints_equal = [2, 24]
+
+# objective_function = [-1, 4, -3, -10]
+# constraints = [[1, 1, -1, 1], [1, 14, 10, -10]]
+# constraints_equal = [0, 11]
+#
+# objective_function = [-1, 5, 1, -1]
+# constraints = [[1, 3, 3, 1], [2, 0, 3, -1]]
+# constraints_equal = [3, 4]
+#
+# objective_function = [-1, -10, 1, -5]
+# constraints = [[1, 2, -1, -1], [-1, 2, 3, 1], [1, 5, 1, -1]]
+# constraints_equal = [1, 2, 5]
+# #
+# objective_function = [-1, -1, -1, -1]
+# constraints = [[4, 2, 5, -1], [5, 3, 6, -2], [3, 2, 4, -1]]
+# constraints_equal = [5, 5, 4]
+
+# objective_function = [-1, -1, 1, -1, 2]
+# constraints = [[8, 1, 1, 1, -2], [6, 1, 2, 3, -4], [10, 1, 3, 6, -7]]
+# constraints_equal = [10, 20, 30]
+
+# objective_function = [-1, -2, -1, 2, -1, 2]
+# constraints = [[1, -1, 1, -1, 1, -1], [2, 3, -2, -3, 2, 3], [3, 2, -1, -4, 3, 2, 10]]
+# constraints_equal = [7, 0, 10]
+
+# objective_function = [-2,-3,-4]
+# constraints = [[3,2,1],[2,5,3]]
+# constraints_equal = [10,15]
+
+
+
+
+
 class Simplex(QWidget):
 
     def __init__(self):
         super().__init__()
         self.initUI()
 
-
     def initUI(self):
         self.objectiveBtn()
-        self.constrainsBtn()
+        self.constraintsBtn()
+        self.constraintsEqualBtn()
         self.solveBtn()
-
-        self.resize(480, 300)
-        self.setWindowTitle('Simplex solver')
+        self.helpBtn()
+        # self.showFullScreen()
+        # self.setFixedSize(self.size())
+        self.setWindowTitle('Optimization problem')
         self.show()
 
     def objectiveBtn(self):
@@ -24,63 +66,93 @@ class Simplex(QWidget):
         self.objective_btn.move(20, 20)
         self.objective_btn.clicked.connect(self.objectiveDialog)
         self.objective_line = QTextBrowser(self)
-        self.objective_line.setGeometry(250, 28, 250, 28)
+        self.objective_line.setGeometry(500, 28, 500, 28)
         self.objective_line.move(200, 20)
 
-    def constrainsBtn(self):
-        self.constrains_btn = QPushButton('Constrains', self)
-        self.constrains_btn.setToolTip("Press to enter the constrains coefficients")
-        self.constrains_btn.move(20, 60)
-        self.constrains_btn.clicked.connect(self.constrainsDialog)
-        self.constrains_line = QTextBrowser(self)
-        self.constrains_line.setGeometry(250, 28, 250, 28)
-        self.constrains_line.move(200, 60)
+    def constraintsBtn(self):
+        self.constraints_btn = QPushButton('Constrains', self)
+        self.constraints_btn.setToolTip("Press to enter the constrains coefficients")
+        self.constraints_btn.move(20, 60)
+        self.constraints_btn.clicked.connect(self.constraintsDialog)
+        self.constraints_line = QTextBrowser(self)
+        self.constraints_line.setGeometry(500, 28, 500, 28)
+        self.constraints_line.move(200, 60)
 
-    def objectiveDialog(self):
-        text, ok = QInputDialog.getText(self, 'Input Dialog', 'Enter objective function:')
-        if ok and self.checkObjectiveFunction(str(text)):
-            global objective_function
-            objective_function = [int(elem) for elem in text.split(',')]
-            self.objective_line.setText(str(text))
-
-    def constrainsDialog(self):
-        text, ok = QInputDialog.getText(self, 'Input Dialog', 'Enter constrains:')
-        if ok:
-            self.constrains_line.setText(str(text))
-            global constrains
-            constrains = [[int(el) for el in elem.split(',')] for elem in text.split(';')]
+    def constraintsEqualBtn(self):
+        self.constraints_equal_btn = QPushButton('Constrains equal', self)
+        self.constraints_equal_btn.setToolTip("Press to enter the constrains equal coefficients")
+        self.constraints_equal_btn.move(20, 100)
+        self.constraints_equal_btn.clicked.connect(self.constraintsEqualDialog)
+        self.constraints_equal_line = QTextBrowser(self)
+        self.constraints_equal_line.setGeometry(500, 28, 500, 28)
+        self.constraints_equal_line.move(200, 100)
 
     def solveBtn(self):
         self.solve_btn = QPushButton('Solve', self)
         self.solve_btn.setToolTip("Press to solve optimization problem")
-        self.solve_btn.move(20, 100)
+        self.solve_btn.move(20, 140)
         self.solve_btn.clicked.connect(self.solveProblem)
-
-        """test textBox"""
         self.solve_line = QTextBrowser(self)
-        self.solve_line.setGeometry(250, 180, 250, 180)
-        self.solve_line.move(200, 100)
+        self.solve_line.setGeometry(500, 140, 500, 140)
+        self.solve_line.move(200, 140)
+
+    def helpBtn(self):
+        self.help_btn = QPushButton('Help', self)
+        self.help_btn.setToolTip("Press to open help of the program")
+        self.help_btn.move(20, 180)
+        self.help_btn.clicked.connect(self.helpDialog)
+
+    def objectiveDialog(self):
+        text, ok = QInputDialog.getText(self, 'Input Dialog', 'Enter objective function:')
+        if ok and self.checkSimpleList(str(text)):
+            global objective_function
+            objective_function = [float(elem) for elem in text.split(',')]
+            # text = ''
+            # for i in range(len(objective_function)):
+            #     text += '{:+g}*x_{}'.format(objective_function[i], i)
+            # text += ' -> min'
+            self.objective_line.setText(str(text))
+
+    def constraintsDialog(self):
+        text, ok = QInputDialog.getText(self, 'Input Dialog', 'Enter constrains:')
+        if ok and self.checkList(str(text)):
+            global constraints
+            constraints = [[float(el) for el in elem.split(',')] for elem in text.split(';')]
+            self.constraints_line.setText(str(text))
+
+    def constraintsEqualDialog(self):
+        text, ok = QInputDialog.getText(self, 'Input Dialog', 'Enter constrains equals:')
+        if ok and self.checkSimpleList(str(text)):
+            global constraints_equal
+            constraints_equal = [float(elem) for elem in text.split(',')]
+            self.constraints_equal_line.setText(str(text))
+
+    def helpDialog(self):
+        QMessageBox.information(self, "Optimization problem",
+                                "Read more information on https://en.wikipedia.org/wiki/Optimization_problem")
 
     def solveProblem(self):
-        objective_function = None
-        constrains = None
-        print("ob_f ", objective_function)
-        print("cons ", constrains)
-        c = [-2, -1, -3, -1]
-        A = [[1, 2, 5, -1], [1, -1, -1, 2]]
-        b = [4, 1]
-        x0_bnds = (None, None)
-        x1_bnds = (None, None)
-        x2_bnds = (None, None)
-        x3_bnds = (None, None)
-        res = linprog(c, A, b, bounds=(x0_bnds, x1_bnds, x2_bnds, x3_bnds))
-        print("res ", res)
-
-        self.solve_line.setText(str(res))
-
-    def checkObjectiveFunction(self, text):
         try:
-            temp = [int(elem) for elem in text.split(',')]
+            res = linprog(objective_function, constraints, constraints_equal)
+            message = res.message
+            if res.status == 0:
+                message += "\nOptimum value for the objective function is {}".format(res.fun)
+                message += '\nThe point at which the value has its minimum is {}'.format(tuple(res.x))
+                print(res)
+        except Exception:
+            message = 'The system of conditions contradictory objectives'
+        self.solve_line.setText(str(message))
+
+    def checkSimpleList(self, text):
+        try:
+            temp = [float(elem) for elem in text.split(',')]
+        except Exception:
+            return False
+        return True
+
+    def checkList(self, text):
+        try:
+            temp = [[float(el) for el in elem.split(',')] for elem in text.split(';')]
         except Exception:
             return False
         return True
