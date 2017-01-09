9f62c252f3980aab6c698f2ebeefd0de9d660d45
Chudakov <ChudakovVit@yandex.ru>
2017-01-10 01:49:43 +0300

    work

--- main.py
+++ main.py
@@ -2,46 +2,9 @@ import sys
 from scipy.optimize import linprog
 from PyQt5.QtWidgets import *
 
-
-objective_function = [-1, 10, -1]
-constraints = [[1, -5.5, -7], [1, 14.5, 7]]
-constraints_equal = [-13, 15]
-
-# objective_function = [-1, -2, -3, 4]
-# constraints = [[1, 1, -1, 1], [1, 14, 10, -10]]
-# constraints_equal = [2, 24]
-
-# objective_function = [-1, 4, -3, -10]
-# constraints = [[1, 1, -1, 1], [1, 14, 10, -10]]
-# constraints_equal = [0, 11]
-#
-# objective_function = [-1, 5, 1, -1]
-# constraints = [[1, 3, 3, 1], [2, 0, 3, -1]]
-# constraints_equal = [3, 4]
-#
-# objective_function = [-1, -10, 1, -5]
-# constraints = [[1, 2, -1, -1], [-1, 2, 3, 1], [1, 5, 1, -1]]
-# constraints_equal = [1, 2, 5]
-# #
-# objective_function = [-1, -1, -1, -1]
-# constraints = [[4, 2, 5, -1], [5, 3, 6, -2], [3, 2, 4, -1]]
-# constraints_equal = [5, 5, 4]
-
-# objective_function = [-1, -1, 1, -1, 2]
-# constraints = [[8, 1, 1, 1, -2], [6, 1, 2, 3, -4], [10, 1, 3, 6, -7]]
-# constraints_equal = [10, 20, 30]
-
-# objective_function = [-1, -2, -1, 2, -1, 2]
-# constraints = [[1, -1, 1, -1, 1, -1], [2, 3, -2, -3, 2, 3], [3, 2, -1, -4, 3, 2, 10]]
-# constraints_equal = [7, 0, 10]
-
-# objective_function = [-2,-3,-4]
-# constraints = [[3,2,1],[2,5,3]]
-# constraints_equal = [10,15]
-
-
-
-
+objective_function = [1, -2, 3]
+constraints = [[1, 2, 3], [-3, -2, -1]]
+constraints_equal = [7, 6]
 
 class Simplex(QWidget):
 
@@ -66,6 +29,8 @@ class Simplex(QWidget):
         self.objective_btn.move(20, 20)
         self.objective_btn.clicked.connect(self.objectiveDialog)
         self.objective_line = QTextBrowser(self)
+        self.objective_line.append("<span style='color:#7A7A7A'>+1*x_0-2*x_1+3*x_2 -> min")
+        self.objective_line.setToolTip("Enter in format: 1, -2, 3")
         self.objective_line.setGeometry(500, 28, 500, 28)
         self.objective_line.move(200, 20)
 
@@ -75,7 +40,11 @@ class Simplex(QWidget):
         self.constraints_btn.move(20, 60)
         self.constraints_btn.clicked.connect(self.constraintsDialog)
         self.constraints_line = QTextBrowser(self)
-        self.constraints_line.setGeometry(500, 28, 500, 28)
+        self.constraints_line.append("<span style='color:#7A7A7A'>+1*x_0+2*x_1+3*x_2 = 7")
+        self.constraints_line.append("<span style='color:#7A7A7A'>-3*x_0-2*x_1-1*x_2 = 6")
+        self.constraints_line.setToolTip("Enter constraints in format: 1, 2, 3; -3, -2, -1 \n" +
+                                         "Enter constraints_equal in format: 7, 6")
+        self.constraints_line.setGeometry(500, 68, 500, 68)
         self.constraints_line.move(200, 60)
 
     def constraintsEqualBtn(self):
@@ -83,9 +52,6 @@ class Simplex(QWidget):
         self.constraints_equal_btn.setToolTip("Press to enter the constrains equal coefficients")
         self.constraints_equal_btn.move(20, 100)
         self.constraints_equal_btn.clicked.connect(self.constraintsEqualDialog)
-        self.constraints_equal_line = QTextBrowser(self)
-        self.constraints_equal_line.setGeometry(500, 28, 500, 28)
-        self.constraints_equal_line.move(200, 100)
 
     def solveBtn(self):
         self.solve_btn = QPushButton('Solve', self)
@@ -107,10 +73,10 @@ class Simplex(QWidget):
         if ok and self.checkSimpleList(str(text)):
             global objective_function
             objective_function = [float(elem) for elem in text.split(',')]
-            # text = ''
-            # for i in range(len(objective_function)):
-            #     text += '{:+g}*x_{}'.format(objective_function[i], i)
-            # text += ' -> min'
+            text = ''
+            for i in range(len(objective_function)):
+                text += '{:+g}*x_{}'.format(objective_function[i], i)
+            text += ' -> min'
             self.objective_line.setText(str(text))
 
     def constraintsDialog(self):
@@ -118,6 +84,11 @@ class Simplex(QWidget):
         if ok and self.checkList(str(text)):
             global constraints
             constraints = [[float(el) for el in elem.split(',')] for elem in text.split(';')]
+            text = ''
+            for i in range(len(constraints)):
+                for j in range(len(constraints[i])):
+                    text += '{:+g}*x_{}'.format(constraints[i][j], j)
+                text += ' = {}\n'.format(constraints_equal[i])
             self.constraints_line.setText(str(text))
 
     def constraintsEqualDialog(self):
@@ -125,20 +96,22 @@ class Simplex(QWidget):
         if ok and self.checkSimpleList(str(text)):
             global constraints_equal
             constraints_equal = [float(elem) for elem in text.split(',')]
-            self.constraints_equal_line.setText(str(text))
 
     def helpDialog(self):
         QMessageBox.information(self, "Optimization problem",
-                                "Read more information on https://en.wikipedia.org/wiki/Optimization_problem")
+                                "Read more information on https://en.wikipedia.org/wiki/Optimization_problem\n" +
+                                "Trial version, expected to improve.\n" +
+                                "Made by Chudakov Vitaly and SciPy")
 
     def solveProblem(self):
         try:
             res = linprog(objective_function, constraints, constraints_equal)
+            if res.slack != [0., 0.]:
+                res.status = 2
             message = res.message
             if res.status == 0:
                 message += "\nOptimum value for the objective function is {}".format(res.fun)
                 message += '\nThe point at which the value has its minimum is {}'.format(tuple(res.x))
-                print(res)
         except Exception:
             message = 'The system of conditions contradictory objectives'
         self.solve_line.setText(str(message))
